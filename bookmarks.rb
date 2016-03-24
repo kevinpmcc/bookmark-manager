2938c190774821cfb5cd7d9c8c87ad614ed7e3dd
Misa Ogura <misa.ogura01@gmail.com>
2016-03-24 14:28:00 +0000

    Enable session, helper method implemented

--- bookmarks.rb
+++ bookmarks.rb
@@ -6,56 +6,58 @@ require_relative 'models/password_encryption'
 
 class Bookmarks < Sinatra::Base
 
-enable :sessions
+use Rack::Session::Pool, :expire_after => 2592000
 
-# get '/' do
-#   redirect '/links/index'
-# end
-get '/' do
-  erb :home
-end
-
-post '/' do
-  my_password = BCrypt::Password.create(params[:password])
-
-  user = User.create(
-    :name => params[:name],
-    :email => params[:email],
-    :password => my_password
-  )
-  user.save
-  p user
-  redirect '/links'
-end
-
-get '/links' do
-  !!User.last ? @name = User.first.name : @name = 'Guest'
-  @links = Link.all
-  erb :'links/index'
-end
-
-get '/links/new' do
-  erb :'links/new'
-end
-
-post '/links/newlink' do
-  link = Link.create(
-  :title => params[:title],
-  :url => params[:url]
-  )
-  params[:tags].split(",").each do |tag|
-    tag = Tag.create(name: tag)
-    link.tags << tag
-  end
-  link.save
-  redirect('/links')
-end
-
-get '/tags/:name' do
-  tag = Tag.first(name: params[:name])
-  @links = tag ? tag.links : []
-  erb :'links/index'
+  helpers do
+    def encrypt(password)
+      BCrypt::Password.create(password)
+    end
   end
 
-run! if app_file ==$0
+  get '/' do
+    erb :home
+  end
+
+  post '/' do
+    session[:name] = params[:name]
+    session[:email] = params[:email]
+    session[:password] = encrypt(params[:password])
+    user = User.create(
+      :name => session[:name],
+      :email => session[:email],
+      :password => session[:password]
+    )
+    redirect '/links'
+  end
+
+  get '/links' do
+    !!User.last ? @name = User.last.name : @name = 'Guest'
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links/newlink' do
+    link = Link.create(
+    :title => params[:title],
+    :url => params[:url]
+    )
+    params[:tags].split(",").each do |tag|
+      tag = Tag.create(name: tag)
+      link.tags << tag
+    end
+    link.save
+    redirect('/links')
+  end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+    end
+
+  run! if app_file ==$0
 end
